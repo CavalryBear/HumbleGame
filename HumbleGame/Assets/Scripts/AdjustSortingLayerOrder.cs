b5c4eb231c811648dbe0e64c3a7adaceb4e4f19f
aiartsev <aleiartsev@gmail.com>
2015-07-18 17:23:55 -0300

    AdjustOrderInLayer Script
    Plus Minion and Warlord Prefabs

---
+++ HumbleGame/Assets/Scripts/AdjustSortingLayerOrder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script updates the Order In Layer of the SpriteRenderer of its gameObject to reflect its position on the y-axis.
+/// </summary>
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AdjustSortingLayerOrder : MonoBehaviour {
+
+	private SpriteRenderer _spriteRenderer;
+
+	/// <summary>
+	/// Get a reference to the SpriteRenderer on Awake
+	/// </summary>
+	void Awake() {
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	/// <summary>
+	/// Update the Order In Layer of the SpriteRenderer every frame
+	/// </summary>
+	void Update () {
+		_spriteRenderer.sortingOrder = (int)(-100 * _spriteRenderer.bounds.min.y);
+	}
+}

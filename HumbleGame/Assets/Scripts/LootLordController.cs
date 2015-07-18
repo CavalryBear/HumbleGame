552a3b1c09cd54dcae1fb41ced4febcf4e485970
aiartsev <aleiartsev@gmail.com>
2015-07-18 18:15:14 -0300

    LootLordController
    Right and Left Movement, Scale switching

---
+++ HumbleGame/Assets/Scripts/LootLordController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class LootLordController : MonoBehaviour {
+	public float maxSpeed = 10f;
+
+	private bool _facingRight = true;
+	private Rigidbody2D _rigidBody;
+
+	// Use this for initialization
+	void Awake () {
+		_rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate () {
+		float moveInput = Input.GetAxis("Horizontal");
+
+		_rigidBody.velocity = new Vector2(moveInput * maxSpeed, _rigidBody.velocity.y);
+
+		if (moveInput > 0 && !_facingRight) {
+			flip();
+		} else if (moveInput < 0 && _facingRight) {
+			flip();
+		}
+	}
+
+	private void flip() {
+		_facingRight = !_facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}

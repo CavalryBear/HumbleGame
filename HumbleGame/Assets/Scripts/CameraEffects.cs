8ead6465e729f5eae9dbe936e2e902dd56da894c
aiartsev <aleiartsev@gmail.com>
2015-08-01 13:11:34 -0300

    Camera Shake Effects
    First implementation of camera effects component with shake effect.

---
+++ HumbleGame/Assets/Scripts/CameraEffects.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEffects : MonoBehaviour {
+
+	public float shakeDuration;
+	public float shakeAmount;
+	public float decreaseFactor;
+
+	private Vector3 _originalPosition;
+
+	void Awake() {
+		_originalPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shakeDuration > 0) {
+			transform.position = _originalPosition + (Vector3)(Random.insideUnitCircle * shakeAmount);
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		} else {
+			shakeDuration = 0f;
+			transform.position = _originalPosition;
+		}
+	}
+
+	public void Shake(float amount) {
+		shakeDuration = amount;
+	}
+}
